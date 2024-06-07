c3b013e872671b20feb2fecd3d42c8f7e2f4bdac
dr-fusion <bruce@fusionx.finance>
2024-06-07 18:48:56 +0530

    change metaimage -t

--- src/config/mantle/production.ts
+++ src/config/mantle/production.ts
@@ -4,7 +4,7 @@ import { commonConfig } from './common';
 const config: AppConfig = {
   ...commonConfig,
   mode: 'production',
-  sentryDSN:
-    'https://692b778079de184efba3b95fd493a72a@o4505549859061760.ingest.us.sentry.io/4507033812140037',
+  // sentryDSN:
+  //   'https://692b778079de184efba3b95fd493a72a@o4505549859061760.ingest.us.sentry.io/4507033812140037',
 };
 export default config;
